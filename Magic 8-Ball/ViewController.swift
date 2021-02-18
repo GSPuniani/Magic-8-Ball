3c5997bf2a6907df64c1b7c314fc08ae3d6fba47
Gobind Puniani <gobindpuniani@gmail.com>
2021-02-17 21:52:25 -0800

    Added IBOutlets for label and button

--- Magic 8-Ball/ViewController.swift
+++ Magic 8-Ball/ViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var answerLabel: UILabel!
+    @IBOutlet weak var shakeButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
