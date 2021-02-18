ed407fb2f245b7caf431dd0ac0fe2b8a303d616e
Gobind Puniani <gobindpuniani@gmail.com>
2021-02-17 22:13:22 -0800

    Added array of answers and implemented shake functionality

--- Magic 8-Ball/ViewController.swift
+++ Magic 8-Ball/ViewController.swift
@@ -8,9 +8,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-    // MARK: - Properties
 
-        let answers = ["Yes, definitely", "It is certain", "Without a doubt", "Yes", "Most likely", "Sure, why not?", "Same", "Tell me more", "Out to lunch", "Reply hazy", "Try Again", "Ask again later", "The cake is a lie", "42", "TMI", "Very doubtful", "Don't count on it", "My reply is no", "Absolutely not", "Possibly", "Maybe", "Probably not"]
+    let answers = ["Yes, definitely", "It is certain", "Without a doubt", "Yes", "Most likely", "Sure, why not?", "Same", "Tell me more", "Out to lunch", "Reply hazy", "Try Again", "Ask again later", "The cake is a lie", "42", "TMI", "Very doubtful", "Don't count on it", "My reply is no", "Absolutely not", "Possibly", "Maybe", "Probably not"]
+    
+    func generateAnswer() {
+        // Pick a random index
+        let randomIndex = Int.random(in: 0..<answers.count)
+        
+        // Assign the answer text to a random answer
+        answerLabel.text = answers[randomIndex]
+    }
 
     @IBOutlet weak var answerLabel: UILabel!
     @IBOutlet weak var shakeButton: UIButton!
@@ -19,12 +26,13 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     @IBAction func shakeButtonTapped(_ sender: Any) {
-//        Pick a random index
-        let randomIndex: Int = Int.random(in: 0..<answers.count)
-        
-        
-//        Assign the answer text to a random answer
-        answerLabel.text = answers[randomIndex]
+        generateAnswer()
+    }
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        guard motion == .motionShake else { return }
+
+        generateAnswer()
     }
     
 
